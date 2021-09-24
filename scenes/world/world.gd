fdb09e8796d96a560dcd9ad5834476b957ac2961
Emily <tweetychrip@gmail.com>
2021-09-24 16:41:29 -0700

    Added a timer

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -1,6 +1,11 @@
 extends Node
 
 
+onready var _dialogue := $UI/Dialogue
+onready var _text := $UI/Dialogue/Text
+onready var _text_timer := $UI/Dialogue/Timer
+
+
 func _ready() -> void:
 	for i in get_tree().get_nodes_in_group("interactable"):
 		i.connect("show_dialogue", self, "_on_dialogue_triggered")
@@ -8,8 +13,12 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and $UI/Dialogue.visible:
-		$UI/Dialogue.hide()
-		$Player.can_move = true
+		if _text.percent_visible == 1.0:
+			_dialogue.hide()
+			$Player.can_move = true
+		else:
+			_text_timer.stop()
+			_text.percent_visible = 1.0
 
 
 func _on_pause_pressed() -> void:
@@ -17,5 +26,13 @@ func _on_pause_pressed() -> void:
 
 
 func _on_dialogue_triggered(dialogue: String) -> void:
-	$UI/Dialogue.show()
-	$UI/Dialogue/Text.bbcode_text = dialogue
+	_dialogue.show()
+	_text.bbcode_text = dialogue
+	_text.visible_characters = 0
+	_text_timer.start()
+
+
+func _on_text_timer_timeout() -> void:
+	_text.visible_characters += 1
+	if not _text.percent_visible == 1.0:
+		_text_timer.start()
