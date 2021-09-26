c0bb4403f84693e989c75f3afa8363b3a8a944e7
Manuel Erdmann <kekseggkeks@googlemail.com>
2021-09-26 21:28:39 +0200

    Stuff

--- scenes/minigames/Life.gd
+++ scenes/minigames/Life.gd
@@ -9,5 +9,5 @@ func _on_Timer_timeout() -> void:
 	$ProgressBar.value -= 1
 	if $ProgressBar.value <= 0:
 		emit_signal("lose")
-	elif $ProgressBar.value >= 100:
+	elif $ProgressBar.value >= 98:
 		emit_signal("win")
